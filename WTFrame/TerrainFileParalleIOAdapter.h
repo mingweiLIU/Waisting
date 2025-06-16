f4d712c333a20df725ccefacb5779fb34b6de4f8
mingweliu <491453302@qq.com>
2025-06-16 22:31:29 +0800

    开始写quantifiedmesh结构体

--- WTFrame/TerrainFileParalleIOAdapter.h
+++ WTFrame/TerrainFileParalleIOAdapter.h
@@ -13,6 +13,88 @@ namespace WT {
 	namespace tbb {
 		class global_control;
 	}
+	
+	class QuantifiedMeshData {
+	public:
+		struct QuantizedMeshHeader
+		{
+			// The center of the tile in Earth-centered Fixed coordinates.
+			double CenterX;
+			double CenterY;
+			double CenterZ;
+
+			// The minimum and maximum heights in the area covered by this tile.
+			// The minimum may be lower and the maximum may be higher than
+			// the height of any vertex in this tile in the case that the min/max vertex
+			// was removed during mesh simplification, but these are the appropriate
+			// values to use for analysis or visualization.
+			float MinimumHeight;
+			float MaximumHeight;
+
+			// The tile¡¯s bounding sphere.  The X,Y,Z coordinates are again expressed
+			// in Earth-centered Fixed coordinates, and the radius is in meters.
+			double BoundingSphereCenterX;
+			double BoundingSphereCenterY;
+			double BoundingSphereCenterZ;
+			double BoundingSphereRadius;
+
+			// The horizon occlusion point, expressed in the ellipsoid-scaled Earth-centered Fixed frame.
+			// If this point is below the horizon, the entire tile is below the horizon.
+			// See http://cesiumjs.org/2013/04/25/Horizon-culling/ for more information.
+			double HorizonOcclusionPointX;
+			double HorizonOcclusionPointY;
+			double HorizonOcclusionPointZ;
+		};
+
+		struct VertexData
+		{
+			unsigned int vertexCount;
+			unsigned short u[vertexCount];
+			unsigned short v[vertexCount];
+			unsigned short height[vertexCount];
+		};
+		struct IndexData16
+		{
+			unsigned int triangleCount;
+			unsigned short indices[triangleCount * 3];
+		}
+
+		struct IndexData32
+		{
+			unsigned int triangleCount;
+			unsigned int indices[triangleCount * 3];
+		}
+
+		struct EdgeIndices16
+		{
+			unsigned int westVertexCount;
+			unsigned short westIndices[westVertexCount];
+
+			unsigned int southVertexCount;
+			unsigned short southIndices[southVertexCount];
+
+			unsigned int eastVertexCount;
+			unsigned short eastIndices[eastVertexCount];
+
+			unsigned int northVertexCount;
+			unsigned short northIndices[northVertexCount];
+		}
+
+		struct EdgeIndices32
+		{
+			unsigned int westVertexCount;
+			unsigned int westIndices[westVertexCount];
+
+			unsigned int southVertexCount;
+			unsigned int southIndices[southVertexCount];
+
+			unsigned int eastVertexCount;
+			unsigned int eastIndices[eastVertexCount];
+
+			unsigned int northVertexCount;
+			unsigned int northIndices[northVertexCount];
+		}
+	};
 
 
 	// Ç°ÏòÉùÃ÷
